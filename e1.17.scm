1bffaaa8f18cf81c6bf5c158ab7a5d61d049c7bd
leushkin <kirill.leushkin1@gmail.com>
2021-02-21 11:30:28 +0300

    e1.17

---
+++ e1.17.scm
@@ -0,0 +1,8 @@
+(define (double x) (+ x x))
+(define (halve x) (/ x 2))
+
+(define (* a b)
+  (cond ((= b 0) 0)
+        ((= a 0) 0)
+        ((even? b) (double (* a (halve b))))
+        (else (+ a (* a (- b 1))))))
