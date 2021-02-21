716621521db15b2b264e8bbff413f52a1486b480
leushkin <kirill.leushkin1@gmail.com>
2021-02-21 18:56:39 +0300

    e1.18.scm

---
+++ e1.18.scm
@@ -0,0 +1,10 @@
+(define (halve x) (/ x 2))
+(define (double x) (* x 2))
+
+(define (mult-iter acc a b)
+  (cond ((= b 0) acc)
+        ((even? b) (mult-iter acc (double a) (halve b)))
+        (else (mult-iter (+ a acc) a (- b 1)))))
+
+(define (* a b)
+  (mult-iter 0 a b))
